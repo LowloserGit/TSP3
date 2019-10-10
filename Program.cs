3d4e40076b5fe53c853c765a40b2d0d45b6d2e4e
LowloserGit <98miwaa@gmail.com>
2019-10-10 09:55:39 +0200

    Adding files to project

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace Oblig1
+{
+    class Program
+    {
+        //enum ExitCode : int
+        //{
+        //    NoError = 0,
+        //    InvalidInput = 1,
+        //    UnknownError = 10
+        //}
+
+        static Random rnd = new Random();
+        static int citiesSize = 5;
+        static int programLoops = 1;
+        static int[,] cities = new int[citiesSize, citiesSize];
+        static int[,] citiesB = new int[citiesSize, citiesSize];
+        
+        static void Main(string[] args)
+        {
+            int startNode;
+            int resultRandomTSP = 0;
+            int resultIterativeRandomTSP = 0;
+            int resultGreedyTSP = 0;
+
+            Console.WriteLine("Choose start node!: ");
+            startNode = int.Parse(Console.ReadLine());
+            Algorithms myAlgorithm = new Algorithms();
+
+            for (int i = 0; i < programLoops; i++)
+            {
+                fillTable();
+                printCitiesTable();
+                Console.Write("\n");
+                printCitiesTableB();
+
+                Console.Write("\n");
+                Console.WriteLine("Current program iteration: {0}", i + 1);
+                Console.WriteLine("RandomTSP:");
+                resultRandomTSP = resultRandomTSP + myAlgorithm.randomTSP(startNode, cities);
+                Console.WriteLine("RandomIterativeTSP:");
+                resultIterativeRandomTSP = resultIterativeRandomTSP + myAlgorithm.iterativeRandomTSP(startNode, cities);
+                Console.WriteLine("ResultGreedyTSP:");
+                resultGreedyTSP = resultGreedyTSP + myAlgorithm.greedyTSP(startNode, cities);
+                Console.Write("\n");
+            }
+            resultRandomTSP = resultRandomTSP / programLoops;
+            resultIterativeRandomTSP = resultIterativeRandomTSP / programLoops;
+            resultGreedyTSP = resultGreedyTSP / programLoops;
+            Console.WriteLine("Average result RandomTSP: {0}", resultRandomTSP);
+            Console.WriteLine("Average result IterativeRandomTSP: {0}", resultIterativeRandomTSP);
+            Console.WriteLine("Average result GreedyTSP: {0}", resultGreedyTSP);
+
+            printCitiesTable();
+            Console.Write("\n");
+            printCitiesTableB();
+            Console.ReadLine();
+        }//main
+
+        static void fillTable()
+        {
+            for (int i = 0; i < cities.GetLength(0); i++)
+            {
+                for (int j = 0; j < cities.GetLength(0); j++)
+                {
+                    if (i == j)
+                    {
+                        cities[i, j] = -1;
+                        citiesB[i, j] = cities[i,j];
+                    }
+                    else
+                    {
+                        cities[i, j] = rnd.Next(1, 100);//Creates a random number between 1 and 10
+                        citiesB[i, j] = cities[i, j];
+                    }
+                }//for
+            }//for
+        }
+
+       
+        
+        static void printCitiesTable()
+        {
+            for (int i = 0; i < cities.GetLength(0); i++)
+            {
+                for (int j = 0; j < cities.GetLength(0); j++)
+                {
+                    if (j == cities.GetLength(0)-1)
+                    {
+                        Console.Write("|{0} |",cities[i, j]);
+                        Console.Write("\n");
+                    }
+                    else if(i == j){
+                        Console.Write("|{0} ", cities[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write("| {0} ",cities[i, j]);
+                    }
+
+                    
+                }
+            }
+        }//printCitiesTable
+
+        static void printCitiesTableB()
+        {
+            for (int i = 0; i < cities.GetLength(0); i++)
+            {
+                for (int j = 0; j < cities.GetLength(0); j++)
+                {
+                    if (j == cities.GetLength(0) - 1)
+                    {
+                        Console.Write("|{0} |", citiesB[i, j]);
+                        Console.Write("\n");
+                    }
+                    else if (i == j)
+                    {
+                        Console.Write("|{0} ", citiesB[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write("| {0} ", citiesB[i, j]);
+                    }
+
+
+                }
+            }
+        }//printCitiesTable
+
+    }//Class program
+}
---
+++ algorithms.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oblig1
+{
+    class Algorithms
+    {
+        static Random rnd = new Random();
+
+        //public int getCurrentBest()
+        //{
+        //    return currentBest;
+        //}
+
+        public int randomTSP(int startNode, int[,] cities )
+        {
+            //pick a number between 1 and 10
+            //pick the corresponding colonne to the number
+            //if its taken, go again
+            //int currentNode = startNode;
+            //int nextNode;
+            int cost;
+            int totalCost = 0;
+            int[] visited = new int[cities.GetLength(0)];
+            int[] solution = new int[cities.GetLength(0)-1];
+            int currentBest = 0;
+
+            if (startNode < 0 || startNode > cities.GetLength(0))
+            {
+                return -1;
+            }
+
+            visited[0] = startNode;
+
+            //fyller visisted[] med -1, slik at node 0 blir et gyldig valg
+            for (int i = 1; i < visited.Length; i++)
+            {
+                visited[i] = -1;
+            }
+
+            for (int i = 1; i < cities.GetLength(0); i++)//GetLenght() gives the size of one dimension in the array(one row/colonne).
+            {
+                bool done = false;
+
+                while (!done)
+                {
+                    bool found = false;
+
+                    visited[i] = rnd.Next(0, cities.GetLength(0));
+
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (visited[i] == visited[j])
+                        {
+                            found = true;
+                        }
+                        //found = visited[i] == visited[j];
+                    }
+                    done = !found;
+
+                }
+            }
+
+            for (int i = 1; i < visited.Length; i++)
+            {
+                cost = cities[visited[i - 1], visited[i]];
+                solution[i-1] = cost;
+                totalCost = totalCost + cost;
+            }
+
+            currentBest = totalCost;
+            printResult(visited, solution, currentBest);
+
+            return currentBest;
+        }//RandomTSP
+
+        public int iterativeRandomTSP(int startNode, int[,] cities)
+        {
+            int cost;
+            int totalCost;
+            int[] visited = new int[cities.GetLength(0)];
+            int[] solution = new int[cities.GetLength(0) - 1];
+            int currentBest = int.MaxValue;
+            //int improvement = int.MaxValue;
+            int counter = 100;
+            do
+            {
+                totalCost = 0;
+
+                if (startNode < 0 || startNode > cities.GetLength(0))
+                {
+                    return 1;
+                }
+
+                visited[0] = startNode;
+
+                //fyller visisted[] med -1, slik at node 0 blir et gyldig valg
+                for (int i = 1; i < visited.Length; i++)
+                {
+                    visited[i] = -1;
+                }
+
+                for (int i = 1; i < cities.GetLength(0); i++)//GetLenght() gives the size of one dimension in the array(one row/colonne).
+                {
+                    bool done = false;
+
+                    while (!done)
+                    {
+                        bool found = false;
+
+                        visited[i] = rnd.Next(0, cities.GetLength(0));
+
+                        for (int j = 0; j < i; j++)
+                        {
+                            if (visited[i] == visited[j])
+                            {
+                                found = true;
+                            }
+                            //found = visited[i] == visited[j];
+                        }
+                        done = !found;
+
+                    }
+                }
+
+                for (int i = 1; i < visited.Length; i++)
+                {
+                    cost = cities[visited[i - 1], visited[i]];
+                    solution[i - 1] = cost;
+                    totalCost = totalCost + cost;
+                }
+
+                if(totalCost < currentBest)
+                {
+                    //improvement = currentBest - totalCost;
+                    currentBest = totalCost;
+                }
+                //Console.Write("{0}, ", totalCost);
+                counter--;
+
+            } while (counter > 0);
+
+            printResult(visited, solution, currentBest);
+            
+            return currentBest;
+        }//IterativeRandom
+
+        public int greedyTSP(int startNode, int[,] cities)
+        {
+            int currentNode = startNode;
+            int nextNode = 0;
+            //int cost = int.MaxValue;
+            int totalCost = 0;
+            int[] visited = new int[cities.GetLength(0)];
+            int[] solution = new int[cities.GetLength(0) - 1];
+            int currentBest;
+            int amountVisited = 1;
+            int minRow = 0;
+            int minColon = 0;
+
+            visited[0] = currentNode;
+
+            for (int i = 0; i < cities.GetLength(0)-1; i++)
+            {
+                int cost = int.MaxValue;
+                /*cities[i, startNode] = -1;*/
+                for (int j = 1; j <= amountVisited; j++)
+                {
+                    currentNode = visited[j - 1];
+
+                    for (nextNode = 0; nextNode < cities.GetLength(0); nextNode++)
+                    {
+                        if (cities[currentNode,nextNode] < cost && cities[currentNode, nextNode] != -1)
+                        {
+                            cost = cities[currentNode, nextNode];
+                            minRow = currentNode;
+                            minColon = nextNode;
+                        }
+                    }
+                }//For visited
+
+                cities[minRow, minColon] = -1;
+                cities[minColon, minRow] = -1;
+
+                if (minColon != visited[i])
+                {
+                    visited[amountVisited] = minColon;
+                    solution[amountVisited - 1] = cost;
+                    totalCost = totalCost + cost;
+                    amountVisited++;
+                }
+                else
+                {
+                    i--;
+                    cities[currentNode, minColon] = -1;
+                    cities[minColon, currentNode] = -1;
+                }
+            }
+
+            currentBest = totalCost;
+            printResult(visited, solution, currentBest);
+            return currentBest;
+        }//greedyTSP
+
+        public void printResult(int[] visited, int[] solution, int currentBest)
+        {
+            Console.Write("Visited: ");
+            for (int i = 0; i < visited.Length; i++)
+            {
+                Console.Write("| {0} ", visited[i]);
+            }
+            Console.Write("|");
+            Console.Write("\n");
+
+            Console.Write("Solution: ");
+            for (int i = 0; i < solution.Length; i++)
+            {
+                Console.Write("| {0} ", solution[i]);
+            }
+            Console.Write("|");
+            Console.Write("\n");
+            Console.WriteLine("CurrentBest: {0}", currentBest);
+            Console.Write("\n");
+        }
+    }
+}
